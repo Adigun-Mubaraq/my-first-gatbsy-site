23a84000511a45f36b6df874add2f02d801837b6
adigun.mubaraq06 <adigun.mubaraq06@gmail.com>
2021-12-24 14:43:23 +0100

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+  return (
+      <Layout pageTitle="About Me">
+          <p>Hi there. I am a proud creator of this site and i am following the tutorial</p>
+      </Layout>
+  )
+}
+
+export default AboutPage
